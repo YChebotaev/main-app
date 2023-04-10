bd4fbbd822fd7a8631c60d5514a64bee8780bb97
ychebotaev <yury.79120345101@gmail.com>
2023-04-10 22:17:42 +0500

    Main styles fixes

--- src/pages/Main/BuyToken/BuyToken.tsx
+++ src/pages/Main/BuyToken/BuyToken.tsx
@@ -13,7 +13,7 @@ export const BuyToken: FC = () => (
       <div className={classes.number}>75.000.000</div>
       <div className={classes.text}>токенов MAIN готовы к продаже</div>
     </div>
-    <Button className={classes.button2} to="/investments/buy">Купить токен MAIN</Button>
+    <Button unstyled className={classes.button2} to="/investments/buy">Купить токен MAIN</Button>
     <div className={classes.coin1} />
     <div className={classes.coin2} />
   </div>
